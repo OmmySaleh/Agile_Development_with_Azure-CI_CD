3e01fc42170a416c70f3a66b529b340924cd5d14
odl_user_sp_205746 <odl_user_205746@udacityhol.onmicrosoft.com>
2022-08-30 20:04:57 +0000

    add files

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -46,5 +46,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://ommysfirstapp.azurewebsites.net:$PORT/predict
+     -X POST http://devopsworld.azurewebsites.net:$PORT/predict
      #your application name <yourappname>goes here
